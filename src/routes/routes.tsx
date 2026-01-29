a6149966853bcc548897282a88928146700f408b
Dayer <dayerxr.cher@gmail.com>
2026-01-30 00:48:48 +0800

    chore: removed irrelevant icons

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,30 @@
 import { createBrowserRouter } from 'react-router';
+import { HomePage } from '../pages/HomePage.tsx';
+import { LoginPage } from '../pages/LoginPage.tsx';
+import { SignupPage } from '../pages/SignupPage.tsx';
+import { DashboardPage } from '../pages/Dashboard.tsx';
+import { CreateBlogPage } from '../pages/CreateBlogPage.tsx';
 
 export const router = createBrowserRouter([
-
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/log-in",
+        element: <LoginPage />,
+    },
+    {
+        path: "/sign-up",
+        element: <SignupPage />,
+    },
+    {
+        path: "/dashboard",
+        element: <DashboardPage />,
+    },
+    {
+        path: "/create-blog",
+        element: <CreateBlogPage />
+    }
+    
 ])
