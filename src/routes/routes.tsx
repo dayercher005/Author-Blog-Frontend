38b9feea898d35505c15856618e16ab0d3a3d2f9
Dayer <dayerxr.cher@gmail.com>
2026-02-10 18:04:57 +0800

    feat: added dependencies and more routing in react-router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,27 +4,49 @@ import { LoginPage } from '../pages/LoginPage.tsx';
 import { SignupPage } from '../pages/SignupPage.tsx';
 import { DashboardPage } from '../pages/Dashboard.tsx';
 import { CreateBlogPage } from '../pages/CreateBlogPage.tsx';
+import { BlogViewer } from '../pages/BlogViewer.tsx';
+import { NotFound } from '../pages/NotFound.tsx';
+import { CommentViewer } from '../pages/CommentViewer.tsx';
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />,
+        errorElement: <NotFound />
     },
     {
         path: "/log-in",
         element: <LoginPage />,
+        errorElement: <NotFound />
     },
     {
         path: "/sign-up",
         element: <SignupPage />,
+        errorElement: <NotFound />
     },
     {
         path: "/dashboard",
         element: <DashboardPage />,
+        errorElement: <NotFound />
+    },
+    {   
+        path: "/dashboard/:blogID",
+        element: <BlogViewer />,
+        errorElement: <NotFound />
+    },
+    {
+        path: "/dashboard/:blogID/:commentID",
+        element: <CommentViewer />,
+        errorElement: <NotFound />
     },
     {
         path: "/create-blog",
-        element: <CreateBlogPage />
+        element: <CreateBlogPage />,
+        errorElement: <NotFound />
+    },
+    {
+        path: "*",
+        element: <NotFound />,
     }
     
 ])
